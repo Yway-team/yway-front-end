9e7c5d18899a9c8e17f323ac4a2b5dd24e941bb4
Fletcher Ramee <fletcher.ramee@stonybrook.edu>
2021-10-29 01:16:52 -0400

    Implement GET_USER query

---
+++ src/graphql/user-queries.js
@@ -0,0 +1,33 @@
+import { gql } from '@apollo/client';
+
+export const GET_USER = gql`
+    query GetUser($_id: String!) {
+        getUser(_id: $_id) {
+            _id
+            googleId
+            username
+            bio
+            avatar
+            privacySettings
+            playPoints
+            creatorPoints
+            moderator
+            friends
+            favorites
+            quizzes
+            platforms
+            achievements {
+              name
+            }
+            drafts {
+              name
+            }
+            history {
+              name
+            }
+            notifications {
+              name
+            }
+        }
+    }
+`;
