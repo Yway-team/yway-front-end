e4cd6aedbe44999bc09e115094fdefa18edee8c7
Tyler Htet Naing Phyo <42888726+TylerHNP@users.noreply.github.com>
2021-11-02 13:47:36 -0400

    merge build 3 into master

--- src/screens/index.js
+++ src/screens/index.js
@@ -1,6 +1,15 @@
+<<<<<<< HEAD
 export { default as HighlightsScreen } from './HighlightsScreen';
 export { default as TopPlatformsScreen } from './TopPlatformsScreen';
 export { default as TopQuizzesScreen } from './TopQuizzesScreen';
 export { default as CreateScreen } from './CreateScreen';
 export { default as FavoritesScreen } from './FavoritesScreen';
 export { default as ProfileScreen } from './ProfileScreen';
+=======
+export { default as HighlightsScreen } from './HighlighsScreen';
+export { default as CreateScreen } from './CreateScreen';
+export { default as FavoritesScreen } from './FavoritesScreen';
+export { default as ProfileScreen } from './ProfileScreen';
+export { default as TopPlatformsScreen } from './TopPlatformsScreen';
+export { default as TopQuizzesScreen } from './TopQuizzesScreen';
+>>>>>>> build3
