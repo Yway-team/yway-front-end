8558540346b49d68895628b63fa144832a22dd78
Fletcher Ramee <fletcher.ramee@stonybrook.edu>
2021-11-03 21:12:49 -0400

    Implement UpdatePrivacySettings mutation

--- src/controllers/graphql/user-mutations.js
+++ src/controllers/graphql/user-mutations.js
@@ -18,3 +18,10 @@ export const LOGIN = gql`
         }
     }
 `;
+
+export const UPDATE_PRIVACY_SETTINGS = gql`
+    mutation UpdatePrivacySettings($privacySettings: String!) {
+        # If privacy settings is not one of 'private', 'public', or 'friends', it does nothing and returns null. On success, returns the new privacy settings.
+        updatePrivacySettings(privacySettings: $privacySettings)
+    }
+`;
