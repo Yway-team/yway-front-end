5428cacba714704c78e6750be685d3d7c52accb2
Fletcher Ramee <fletcher.ramee@stonybrook.edu>
2021-11-03 20:42:17 -0400

    Refactor queries, remove GetUser

--- src/graphql/user-queries.js
+++ src/graphql/user-queries.js
@@ -1,40 +1,8 @@
 import { gql } from '@apollo/client';
 
-export const GET_USER = gql`
-    query GetUser($_id: String!) {
-        getUser(_id: $_id) {
-            _id
-            googleId
-            username
-            bio
-            avatar
-            privacySettings
-            playPoints
-            creatorPoints
-            moderator
-            friends
-            favorites
-            quizzes
-            platforms
-            achievements {
-              name
-            }
-            drafts {
-              name
-            }
-            history {
-              name
-            }
-            notifications {
-              name
-            }
-        }
-    }
-`;
-
 export const GET_USER_PUBLIC_INFO = gql`
-    query GetUserPublicInfo($_id: String!) {
-        getUserPublicInfo(_id: $_id) {
+    query GetUserPublicInfo($userId: ID!) {
+        getUserPublicInfo(userId: $userId) {
             _id
             username
             avatar
@@ -44,7 +12,7 @@ export const GET_USER_PUBLIC_INFO = gql`
 `;
 
 export const GET_USER_INFO = gql`
-    query GetUserInfo($userId: String!) {
+    query GetUserInfo($userId: ID!) {
         getUserInfo(userId: $userId) {
             _id
             username
