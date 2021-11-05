727c36ff2ac32d59f78b14e3465efea3e6c0391b
Tyler Htet Naing Phyo <42888726+TylerHNP@users.noreply.github.com>
2021-11-05 13:13:17 -0400

    updated quiz card

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
-import { Tab, Tabs, Typography, Box, Avatar, Grid, Divider, Button } from '@mui/material';
+import { Tab, Tabs, Typography, Box, Avatar, Grid, Divider, Button, Dialog } from '@mui/material';
 import { Settings, Edit, } from '@mui/icons-material';
 import ProfilePrivacy from './ProfilePrivacy';
 import { useHistory, useRouteMatch, useParams } from 'react-router-dom';
@@ -12,8 +12,17 @@ export default function ProfileScreen() {
   const { userId } = useParams();
   const history = useHistory();
   const [value, setValue] = useState(0);
+  const [settingsOpen, setSettingsOpen] = useState(false);
+
   const routes = ['/overview', '/achievements', '/quizzes', '/platforms', '/history', '/friends'];
 
+
+  const handleClickSettingsOpen = () => {
+    setSettingsOpen(true);
+  };
+  const handleSettingsClose = () => {
+    setSettingsOpen(false);
+  };
   const handleChange = (event, newValue) => {
     setValue(newValue);
     // history.push(url + routes[newValue]);
@@ -30,92 +39,95 @@ export default function ProfileScreen() {
   }, []);
 
   return (
-    <Grid container justifyContent='center' alignItems='center' >
-      <Grid flexDirection='column' sx={{ maxWidth: '1000px' }} >
-        <Grid sx={{ height: "150px", overflow: "hidden", }}>
-          <img alt='cover' src="https://picsum.photos/1000" sx={{ objectFit: 'fill' }} />
-        </Grid>
-        <Grid item container justifyContent='center' flexDirection='column' alignItems='center' >
-          <Avatar alt="avatar" src={userInfo ? userInfo.avatar : null /*"https://i.pravatar.cc/300"*/}
-            sx={{
-              mt: -12,
-              height: 130,
-              width: 130,
-              border: '0.2rem solid',
-              borderColor: 'common.white'
-            }}
-            imgProps={{ style: { borderRadius: '50%', objectFit: 'fill' } }} />
-          <Typography sx={{
-            fontWeight: 700,
-            fontSize: 25,
-            color: 'common.black'
-          }}>
-            {userInfo ? userInfo.username : null}
-          </Typography>
-          <Typography
-            sx={{
-              fontWeight: 500,
-              fontSize: 14,
-              mt: 1
-            }} >
-            {userInfo ? userInfo.bio : null}
-          </Typography>
-          <Divider flexItem sx={{ mt: 3 }} />
-        </Grid>
-        <Grid container sx={{ justifyContent: 'space-between', alignItems: 'center' }}>
-          <Tabs
-            value={value}
-            onChange={handleChange}
-            aria-label="user tabs"
-            textColor='primary'
-            indicatorColor='primary'
-            sx={{
-              [`& .MuiTab-root`]: {
-                fontWeight: 600,
-              }
-            }}
-          >
-            <Tab label="Overview" {...a11yProps(0)} />
-            <Tab label="Achievements" {...a11yProps(1)} />
-            <Tab label="My Quizzes"{...a11yProps(2)} />
-            <Tab label="My Platforms" {...a11yProps(3)} />
-            <Tab label="History"{...a11yProps(4)} />
-            <Tab label="Friends" {...a11yProps(5)} />
-            <Tab label="Settings" {...a11yProps(6)} />
-
-          </Tabs>
-          <Grid item>
-            <Button variant="text" startIcon={<Settings />} sx={{ mr: 1 }}>
-              Settings
-            </Button>
-            <Button variant="contained" startIcon={<Edit />}>
-              Edit Profile
-            </Button>
+    <>
+      <Grid container justifyContent='center' alignItems='center' >
+        <Grid flexDirection='column' sx={{ maxWidth: '1000px' }} >
+          <Grid sx={{ height: "150px", overflow: "hidden", }}>
+            <img alt='cover' src="https://picsum.photos/1000" sx={{ objectFit: 'fill' }} />
           </Grid>
-        </Grid>
+          <Grid item container justifyContent='center' flexDirection='column' alignItems='center' >
+            <Avatar alt="avatar" src={userInfo ? userInfo.avatar : null /*"https://i.pravatar.cc/300"*/}
+              sx={{
+                mt: -12,
+                height: 130,
+                width: 130,
+                border: '0.2rem solid',
+                borderColor: 'common.white'
+              }}
+              imgProps={{ style: { borderRadius: '50%', objectFit: 'fill' } }} />
+            <Typography sx={{
+              fontWeight: 700,
+              fontSize: 25,
+              color: 'common.black'
+            }}>
+              {userInfo ? userInfo.username : null}
+            </Typography>
+            <Typography
+              sx={{
+                fontWeight: 500,
+                fontSize: 14,
+                mt: 1
+              }} >
+              {userInfo ? userInfo.bio : null}
+            </Typography>
+            <Divider flexItem sx={{ mt: 3 }} />
+          </Grid>
+          <Grid container sx={{ justifyContent: 'space-between', alignItems: 'center' }}>
+            <Tabs
+              value={value}
+              onChange={handleChange}
+              aria-label="user tabs"
+              textColor='primary'
+              indicatorColor='primary'
+              sx={{
+                [`& .MuiTab-root`]: {
+                  fontWeight: 600,
+                }
+              }}
+            >
+              <Tab label="Overview" {...a11yProps(0)} />
+              <Tab label="Achievements" {...a11yProps(1)} />
+              <Tab label="My Quizzes"{...a11yProps(2)} />
+              <Tab label="My Platforms" {...a11yProps(3)} />
+              <Tab label="History"{...a11yProps(4)} />
+              <Tab label="Friends" {...a11yProps(5)} />
+              <Tab label="Settings" {...a11yProps(6)} />
 
-        <TabPanel value={value} index={0}>
-          Overview
-        </TabPanel>
-        <TabPanel value={value} index={1}>
-          Achievements
-        </TabPanel>
-        <TabPanel value={value} index={2}>
-          My Quizzes
-        </TabPanel>
-        <TabPanel value={value} index={3}>
-          My Platforms
-        </TabPanel>
-        <TabPanel value={value} index={4}>
-          History
-        </TabPanel>
-        <TabPanel value={value} index={5}>
-          Friends
-        </TabPanel>
-        <TabPanel value={value} index={6}>
-          <ProfilePrivacy userInfo={userInfo} />
-        </TabPanel>
-        {/* <Link to={`${url}/quizzes`}>Props v. State</Link>
+            </Tabs>
+            <Grid item>
+              <Button variant="text" startIcon={<Settings />} sx={{ mr: 1 }}>
+                Settings
+              </Button>
+              <Button variant="contained" startIcon={<Edit />}>
+                Edit Profile
+              </Button>
+            </Grid>
+          </Grid>
+
+          <TabPanel value={value} index={0}>
+            Overview
+          </TabPanel>
+          <TabPanel value={value} index={1}>
+            Achievements
+          </TabPanel>
+          <TabPanel value={value} index={2}>
+            My Quizzes
+          </TabPanel>
+          <TabPanel value={value} index={3}>
+            My Platforms
+          </TabPanel>
+          <TabPanel value={value} index={4}>
+            History
+          </TabPanel>
+          <TabPanel value={value} index={5}>
+            Friends
+          </TabPanel>
+          <TabPanel value={value} index={6}>
+            <ProfilePrivacy userInfo={userInfo} />
+          </TabPanel>
+
+
+          {/* <Link to={`${url}/quizzes`}>Props v. State</Link>
         <Grid>
           <Switch>
             <Route exact path={path}>
@@ -142,8 +154,14 @@ export default function ProfileScreen() {
           </Switch>
         </Grid> */}
 
-      </Grid>
-    </Grid >
+        </Grid>
+      </Grid >
+
+      <Dialog open={settingsOpen} onClose={handleSettingsClose}
+        aria-labelledby="quiz-details-dialog"  >
+
+      </Dialog>
+    </>
   )
 }
 
