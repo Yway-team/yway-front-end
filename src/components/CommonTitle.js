97b9bb74d53034de91f9c5369d2ba4c43e864ffd
Tyler Htet Naing Phyo <42888726+TylerHNP@users.noreply.github.com>
2021-11-05 01:50:55 -0400

    quiz card modal added

--- src/components/CommonTitle.js
+++ src/components/CommonTitle.js
@@ -2,13 +2,14 @@ import { Typography, Grid } from "@mui/material";
 
 function CommonTitle({ title }) {
     return (
-        <Grid xs={12}>
+        <Grid sx={{ width: '100%' }}>
             <Typography sx={{
                 fontWeight: '700',
                 fontSize: 16,
                 color: 'common.black',
                 my: 4,
                 ml: 2,
+
             }
             }> {title}</Typography >
         </Grid >
--- src/screens/TopQuizzesScreen.js
+++ src/screens/TopQuizzesScreen.js
@@ -1,17 +1,98 @@
-import { Box, Typography, Container, Grid } from '@mui/material';
+import { Grid } from '@mui/material';
 
 import { QuizCard, CommonTitle } from "../components";
 
-const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
 
 export default function TopQuizzesScreen() {
     return (
-        <Grid container direction="column" sx={{ alignItems: 'center', justifyContent: 'center', p: 2 }}>
+        <Grid container direction="column" sx={{ alignItems: 'center', justifyContent: 'center', p: 2, pl: 10, }}>
+            <CommonTitle title='TOP QUIZZES' />
             <Grid container justifyContent='flex-start'>
-                <CommonTitle title='TOP QUIZZES' />
-                {cards.map((index) => <QuizCard />)}
+                {quizzes.map((data) => <QuizCard key={data.id} {...data} />)}
             </Grid>
-
         </Grid>
     );
 }
+
+
+const quizzes = [
+    {
+        id: 1,
+        name: 'How much do you know your chinese cuisine',
+        description: "Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorpe eget nulla facilisi etia dignissim diam. Pulvinar elementum integer enim neque volutpat ac tincidunt. Ornare suspendisse sed nisi lacus sed viverra  tellus. Purus sit amet volutpat consequat mauris. Elementum eu facilisis",
+        thumbnail: "https://images.pexels.com/photos/858115/pexels-photo-858115.jpeg",
+        numQuestions: 10,
+        creator: 'yourMama101',
+        creatorImage: "https://i.pravatar.cc/300",
+        rating: 4.5,
+        timestamp: '2 months ago',
+        platformImage: "https://i.pravatar.cc/300",
+        platform: 'Mcdonal123',
+    },
+    {
+        id: 2,
+        name: 'How much do you know your chinese cuisine',
+        description: "Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorpe eget nulla facilisi etia dignissim diam. Pulvinar elementum integer enim neque volutpat ac tincidunt. Ornare suspendisse sed nisi lacus sed viverra  tellus. Purus sit amet volutpat consequat mauris. Elementum eu facilisis",
+        thumbnail: "https://images.pexels.com/photos/858115/pexels-photo-858115.jpeg",
+        numQuestions: 10,
+        creator: 'yourMama101',
+        creatorImage: "https://i.pravatar.cc/300",
+        rating: 4.5,
+        timestamp: '2 months ago',
+        platformImage: "https://i.pravatar.cc/300",
+        platform: 'Mcdonal123',
+    },
+    {
+        id: 3,
+        name: 'How much do you know your chinese cuisine',
+        description: "Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorpe eget nulla facilisi etia dignissim diam. Pulvinar elementum integer enim neque volutpat ac tincidunt. Ornare suspendisse sed nisi lacus sed viverra  tellus. Purus sit amet volutpat consequat mauris. Elementum eu facilisis",
+        thumbnail: "https://images.pexels.com/photos/858115/pexels-photo-858115.jpeg",
+        numQuestions: 10,
+        creator: 'yourMama101',
+        creatorImage: "https://i.pravatar.cc/300",
+        rating: 4.5,
+        timestamp: '2 months ago',
+        platformImage: "https://i.pravatar.cc/300",
+        platform: 'Mcdonal123',
+    },
+    {
+        id: 4,
+        name: 'How much do you know your chinese cuisine',
+        description: "Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorpe eget nulla facilisi etia dignissim diam. Pulvinar elementum integer enim neque volutpat ac tincidunt. Ornare suspendisse sed nisi lacus sed viverra  tellus. Purus sit amet volutpat consequat mauris. Elementum eu facilisis",
+        thumbnail: "https://images.pexels.com/photos/858115/pexels-photo-858115.jpeg",
+        numQuestions: 10,
+        creator: 'yourMama101',
+        creatorImage: "https://i.pravatar.cc/300",
+        rating: 4.5,
+        timestamp: '2 months ago',
+        platformImage: "https://i.pravatar.cc/300",
+        platform: 'Mcdonal123',
+    },
+    {
+        id: 5,
+        name: 'How much do you know your chinese cuisine',
+        description: "Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorpe eget nulla facilisi etia dignissim diam. Pulvinar elementum integer enim neque volutpat ac tincidunt. Ornare suspendisse sed nisi lacus sed viverra  tellus. Purus sit amet volutpat consequat mauris. Elementum eu facilisis",
+        thumbnail: "https://images.pexels.com/photos/858115/pexels-photo-858115.jpeg",
+        numQuestions: 10,
+        creator: 'yourMama101',
+        creatorImage: "https://i.pravatar.cc/300",
+        rating: 4.5,
+        timestamp: '2 months ago',
+        platformImage: "https://i.pravatar.cc/300",
+        platform: 'Mcdonal123',
+    },
+    {
+        id: 6,
+        name: 'How much do you know your chinese cuisine',
+        description: "Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorpe eget nulla facilisi etia dignissim diam. Pulvinar elementum integer enim neque volutpat ac tincidunt. Ornare suspendisse sed nisi lacus sed viverra  tellus. Purus sit amet volutpat consequat mauris. Elementum eu facilisis",
+        thumbnail: "https://images.pexels.com/photos/858115/pexels-photo-858115.jpeg",
+        numQuestions: 10,
+        creator: 'yourMama101',
+        creatorImage: "https://i.pravatar.cc/300",
+        rating: 4.5,
+        timestamp: '2 months ago',
+        platformImage: "https://i.pravatar.cc/300",
+        platform: 'Mcdonal123',
+    },
+];
