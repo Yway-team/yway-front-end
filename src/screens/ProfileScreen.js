7fd0ee23cf225794dbbcc3bd731ca36ec198a6eb
Tyler Htet Naing Phyo <42888726+TylerHNP@users.noreply.github.com>
2021-11-15 14:21:40 -0500

    Revert "added dialog of privacy settings"
    This reverts commit e657c7ff93ee0da47d56e76b55a2f30e6580aea2.

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -91,11 +91,11 @@ export default function ProfileScreen() {
               <Tab label="My Platforms" {...a11yProps(3)} />
               <Tab label="History"{...a11yProps(4)} />
               <Tab label="Friends" {...a11yProps(5)} />
-
+              <Tab label="Settings" {...a11yProps(6)} />
 
             </Tabs>
             <Grid item>
-              <Button variant="text" startIcon={<Settings />} sx={{ mr: 1 }} onClick={handleClickSettingsOpen}>
+              <Button variant="text" startIcon={<Settings />} sx={{ mr: 1 }}>
                 Settings
               </Button>
               <Button variant="contained" startIcon={<Edit />}>
@@ -122,7 +122,9 @@ export default function ProfileScreen() {
           <TabPanel value={value} index={5}>
             Friends
           </TabPanel>
-
+          <TabPanel value={value} index={6}>
+            <ProfilePrivacy userInfo={userInfo} />
+          </TabPanel>
 
 
           {/* <Link to={`${url}/quizzes`}>Props v. State</Link>
@@ -156,8 +158,8 @@ export default function ProfileScreen() {
       </Grid >
 
       <Dialog open={settingsOpen} onClose={handleSettingsClose}
-        aria-labelledby="privacy-settings-dialog"  >
-        <ProfilePrivacy userInfo={userInfo} handleClose={handleSettingsClose} />
+        aria-labelledby="quiz-details-dialog"  >
+
       </Dialog>
     </>
   )
