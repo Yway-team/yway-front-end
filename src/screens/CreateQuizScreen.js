f47eee57fb5fea0caf23f3040406ba6ce6377739
Fletcher Ramee <fletcher.ramee@stonybrook.edu>
2021-12-01 21:54:24 -0500

    Pass image names when saving drafts

--- src/screens/CreateQuizScreen.js
+++ src/screens/CreateQuizScreen.js
@@ -95,7 +95,9 @@ export default function CreateQuizScreen({ draftId }) {
             platformName: quizDetails.platformName,
             timeToAnswer: quizDetails.timeToAnswer,
             bannerImgData: quizDetails.bannerImgData,
+            bannerImgName: quizDetails.bannerImgName,
             thumbnailImgData: quizDetails.thumbnailImgData,
+            thumbnailImgName: quizDetails.thumbnailImgName,
             color: quizDetails.color,
             tags: quizDetails.tags,
             updatedAt: Date.now()
