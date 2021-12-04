73e59dbb1b041f2f21ff4da8e60d4a6bc6be4a42
John Park <loopuntiltrue@gmail.com>
2021-12-04 17:02:20 -0500

    Basic updated to FullLeaderboard

--- src/components/PlatformScreen/MiniLeaderboardRow.js
+++ src/components/PlatformScreen/MiniLeaderboardRow.js
@@ -6,25 +6,6 @@ import Typography from '@mui/material/Typography';
 export default function MiniLeaderboardRow({avatar, score, username, position}) {
     return (
         <>
-            {/* <Grid item xs={1}>
-                <Typography sx={{fontWeight:"bold"}}>    
-                    {position+1}
-                </Typography>
-            </Grid>
-            <Grid item xs={1}>
-                <img style={{height: "20px"}} src={avatar}/>
-            </Grid>
-            <Grid item xs={6}>
-                <Typography>
-                    {username}
-                </Typography>
-            </Grid>
-            <Grid item xs={4}>
-                <Typography sx={{color:"lightgray"}}>
-                    {score} pts
-                </Typography>
-            </Grid> */}
-
             <Grid item xs={1} sx={{fontWeight: "bold"}}>
                     {position+1}
             </Grid>
--- src/screens/FullLeaderboard.js
+++ src/screens/FullLeaderboard.js
@@ -4,10 +4,13 @@ import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Grid from '@mui/material/Grid';
 import Header from '../components/PlatformScreen/Header';
 import { useParams, useHistory } from 'react-router';
 import usePrivilegedQuery from '../hooks/usePrivilegedQuery';
 import { GET_PLATFORM_SUMMARY } from '../controllers/graphql/platform-queries';
+import FullLeaderboardRow from '../components/PlatformScreen/FullLeaderboardRow';
 
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
@@ -52,9 +55,13 @@ export default function FullLeaderboard() {
   const { data: platformData, refetch, error, loading } = usePrivilegedQuery(GET_PLATFORM_SUMMARY, { variables: { title: platformName } });
 
   let platformSummary;
+  let authorized;
+  let leaderboardEntries;
   if (platformData) {
       console.log(platformData)
       platformSummary = platformData.getPlatformSummary;
+      authorized = platformData.getPlatformSummary.moderator
+      leaderboardEntries = platformSummary.leaderboardEntries
   }
 
   const handleChange = (event, newValue) => {
@@ -65,42 +72,63 @@ export default function FullLeaderboard() {
     history.push(`/platform/${platformName}`)
   }
 
+  const sortingFunction = (a,b) => {
+    return b.secondaryScore-a.secondaryScore
+  }
+  if (leaderboardEntries){
+    leaderboardEntries = leaderboardEntries.slice().sort(sortingFunction)
+  }
+
   return (
     <>
-    <Header platformName={platformName}/>
-    <Box sx={{ width: "100%", mt: "100px", minHeight: "600px"}}>
-      <Box sx={{paddingLeft: "5%"}}>
-        <Typography sx={{fontSize:"20px", fontWeight: "bold"}}>
-          Return to Platform
-        </Typography>
-        <Typography sx={{fontSize:"20px", fontWeight: "bold"}}>
-          Leaderboard
-        </Typography>
-      </Box>
-      <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-        <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
-          <Tab label="Top" sx={{fontSize:"12px", minWidth: `${20/2}px`}} {...a11yProps(0)} />
-          <Tab label="Rising" sx={{fontSize:"12px", minWidth: `${20/2}px`}} {...a11yProps(1)} />
-          <Tab label="Past Week" sx={{fontSize:"12px"}} {...a11yProps(2)} />
-          <Tab label="Past Month" sx={{fontSize:"12px"}} {...a11yProps(3)} />
-        </Tabs>
-      </Box>
-      <TabPanel value={value} index={0}>
-        Nobody has played any quizzes yet!
-      </TabPanel>
-      <TabPanel value={value} index={1}>
-        Nobody has played any quizzes yet!
-      </TabPanel>
-      <TabPanel value={value} index={2}>
-        Nobody has played any quizzes yet!
-      </TabPanel>
-      <TabPanel value={value} index={3}>
-        Nobody has played any quizzes yet!
-      </TabPanel>
-      <TabPanel value={value} index={4}>
-        Nobody has played any quizzes yet!
-      </TabPanel>
-    </Box>
+    {platformSummary ? <Header platformName={platformName}
+                thumbnailImg={platformSummary.thumbnailImg}
+                bannerImg={platformSummary.bannerImg}
+                favorites={platformSummary.favorites}
+                numQuestions={platformSummary.numQuestions}
+                numQuizzes={platformSummary.numQuizzes}
+                description={platformSummary.description}
+                tags={platformSummary.tags}
+                color={platformSummary.color}
+                id={platformSummary._id}
+                authorized={authorized}
+            /> : null}
+    {platformSummary&&
+      <Box sx={{ width: "80%", mt: "100px", ml: "50px", minHeight: "600px"}}>
+        <Box sx={{paddingLeft: "5%"}}>
+          <Button sx={{fontSize:"20px", color:"blue"}} onClick={returnToPlatform}>
+            &lt;&lt;Return to Platform
+          </Button>
+          <Typography sx={{fontSize:"20px", fontWeight: "bold"}}>
+            Leaderboard
+          </Typography>
+        </Box>
+        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+            <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
+              <Tab label="Top" sx={{fontSize:"12px", minWidth: `${20/2}px`}} {...a11yProps(0)} />
+              <Tab label="Rising" sx={{fontSize:"12px", minWidth: `${20/2}px`}} {...a11yProps(1)} />
+              <Tab label="Past Week" sx={{fontSize:"12px"}} {...a11yProps(2)} />
+              <Tab label="Past Month" sx={{fontSize:"12px"}} {...a11yProps(3)} />
+            </Tabs>
+          </Box>
+        <TabPanel value={value} index={0}>
+          <Grid container justifyContent="center" spacing={2}>
+            {leaderboardEntries?leaderboardEntries.map((leaderBoardEntry,i)=>{if (i < 10) {return <FullLeaderboardRow key={i} avatar={leaderBoardEntry.avatar} score={leaderBoardEntry.secondaryScore} username={leaderBoardEntry.username} position={i}/>}}):<Grid item xs={12}>Nobody has played any quizzes yet!</Grid>}
+          </Grid>
+        </TabPanel>
+        <TabPanel value={value} index={1}>
+          Nobody has played any quizzes yet!
+        </TabPanel>
+        <TabPanel value={value} index={2}>
+          Nobody has played any quizzes yet!
+        </TabPanel>
+        <TabPanel value={value} index={3}>
+          Nobody has played any quizzes yet!
+        </TabPanel>
+        <TabPanel value={value} index={4}>
+          Nobody has played any quizzes yet!
+        </TabPanel>
+      </Box>}
     </>
   );
 }
