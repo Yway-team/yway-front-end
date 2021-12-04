a10d8ef0962e63f1db2ab39450482be68febf8c0
Tyler Htet Naing Phyo <42888726+TylerHNP@users.noreply.github.com>
2021-12-04 17:36:15 -0500

    added notification error

--- src/components/NavigationControl/NotificationsPopUp.js
+++ src/components/NavigationControl/NotificationsPopUp.js
@@ -35,77 +35,78 @@ function NotificationsPopUp() {
 
 
     return (
-        <>
-            <Box sx={{ position: 'relative' }}>
-                <IconButton id="notification-button"
-                    onClick={handleClickOpen} >
-                    <NotificationsIcon sx={{ width: 25, height: 25 }} />
-                </IconButton>
-                {!read && numberOfNotifications != 0 ?
-                    <Avatar sx={{ position: 'absolute', top: 2, right: -1, height: 20, width: 20, backgroundColor: 'primary.main' }}>
-                        <Typography fontSize={14}>
-                            {numberOfNotifications}
-                        </Typography>
-                    </Avatar> : null
-                }
-
-            </Box>
-            <Menu
-                anchorEl={anchorEl}
-                open={open}
-                onClose={handleClose}
-                onClick={handleClose}
-                PaperProps={{
-                    elevation: 0,
-                    sx: {
-                        overflow: "auto",
-                        width: 350,
-                        height: '60%',
-                        filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
-                        mt: 1,
-                        "& .MuiAvatar-root": {
-                            width: 36,
-                            height: 36,
-                            ml: 0.3,
-                            mr: 1.3
-                        },
-                        '& .MuiMenuItem-root': {
-                            p: '7px',
-                            paddingLeft: '30px'
-                        },
-                        '& .MuiSvgIcon-root': {
-                            ml: 1,
-                            mr: 3,
-                            my: 0.5,
-                            fill: '#858585',
-                            fontSize: 19,
-                        },
-                        "&:before": {
-                            content: '""',
-                            display: "block",
-                            position: "absolute",
-                            top: 0,
-                            right: 14,
-                            width: 10,
-                            height: 10,
-                            bgcolor: "background.paper",
-                            transform: "translateY(-50%) rotate(45deg)",
-                            zIndex: 0
-                        }
+        notifications ?
+            <>
+                <Box sx={{ position: 'relative' }}>
+                    <IconButton id="notification-button"
+                        onClick={handleClickOpen} >
+                        <NotificationsIcon sx={{ width: 25, height: 25 }} />
+                    </IconButton>
+                    {!read && numberOfNotifications != 0 ?
+                        <Avatar sx={{ position: 'absolute', top: 2, right: -1, height: 20, width: 20, backgroundColor: 'primary.main' }}>
+                            <Typography fontSize={14}>
+                                {numberOfNotifications}
+                            </Typography>
+                        </Avatar> : null
                     }
-                }}
-                transformOrigin={{ horizontal: "right", vertical: "top" }}
-                anchorOrigin={{ horizontal: "right", vertical: "bottom" }}>
 
-                <List>
-                    {notifications.map((data) =>
-                        <NotificationCard key={data._id}{...data} />
+                </Box>
+                <Menu
+                    anchorEl={anchorEl}
+                    open={open}
+                    onClose={handleClose}
+                    onClick={handleClose}
+                    PaperProps={{
+                        elevation: 0,
+                        sx: {
+                            overflow: "auto",
+                            width: 350,
+                            height: '60%',
+                            filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
+                            mt: 1,
+                            "& .MuiAvatar-root": {
+                                width: 36,
+                                height: 36,
+                                ml: 0.3,
+                                mr: 1.3
+                            },
+                            '& .MuiMenuItem-root': {
+                                p: '7px',
+                                paddingLeft: '30px'
+                            },
+                            '& .MuiSvgIcon-root': {
+                                ml: 1,
+                                mr: 3,
+                                my: 0.5,
+                                fill: '#858585',
+                                fontSize: 19,
+                            },
+                            "&:before": {
+                                content: '""',
+                                display: "block",
+                                position: "absolute",
+                                top: 0,
+                                right: 14,
+                                width: 10,
+                                height: 10,
+                                bgcolor: "background.paper",
+                                transform: "translateY(-50%) rotate(45deg)",
+                                zIndex: 0
+                            }
+                        }
+                    }}
+                    transformOrigin={{ horizontal: "right", vertical: "top" }}
+                    anchorOrigin={{ horizontal: "right", vertical: "bottom" }}>
 
-                    )}
-                </List>
-            </Menu>
+                    <List>
+                        {notifications.map((data) =>
+                            <NotificationCard key={data._id}{...data} />
 
-        </>)
+                        )}
+                    </List>
+                </Menu>
+
+            </> : <> </>);
 
 }
 
