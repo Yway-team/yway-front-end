182e3ae06ad2c645a2a65b7fd677ab809a0af5b0
Tyler Htet Naing Phyo <42888726+TylerHNP@users.noreply.github.com>
2021-12-04 15:49:52 -0500

    fixed profile screen

--- src/components/ProfileScreen/MyQuizzes.js
+++ src/components/ProfileScreen/MyQuizzes.js
@@ -1,23 +1,24 @@
-import { Grid } from '@mui/material';
+import { Grid, Typography } from '@mui/material';
 import { CommonTitle, QuizCard } from '..';
 import { GET_USER_QUIZZES_INFO } from '../../controllers/graphql/user-queries';
 import usePrivilegedQuery from '../../hooks/usePrivilegedQuery';
 
 export default function MyQuizzes({ userId, isOwn, username }) {
     const { data: quizData, refetch } = usePrivilegedQuery(GET_USER_QUIZZES_INFO, { variables: { userId: userId } });
-    let quizzes = null;
+    let quizzes = [];
     if (quizData) {
         quizzes = quizData.getUserQuizzesInfo;
     }
 
     return (
-        <Grid container direction='column' sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', width: '100%', py: 2 }}>
-            <Grid container justifyContent='flex-start' pl={3}>
+        <Grid container direction='column' sx={{ display: 'flex', justifyContent: 'center', width: '100%', py: 2 }}>
+            <Grid container justifyContent='flex-start' pl={3} >
                 <CommonTitle title={`${isOwn ? 'MY ' : (username.toUpperCase() + '\'S ' || '')}QUIZZES`} />
             </Grid>
             <Grid container justifyContent='flex-start' mb={1}>
                 {quizzes ? quizzes.map((data) => <QuizCard key={data._id} {...data} refetch={refetch} />) : null}
             </Grid>
+            {quizzes.length == 0 ? <Typography pl={3}> {`There is no quiz yet.`} </Typography> : null}
         </Grid >
     );
 }
