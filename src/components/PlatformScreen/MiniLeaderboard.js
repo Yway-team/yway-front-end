751afa00d9f9536751baaf58906b2dbb0d55acd4
John Park <loopuntiltrue@gmail.com>
2021-12-10 19:03:08 -0500

    Removed tabs from minileaderboard

--- src/components/PlatformScreen/MiniLeaderboard.js
+++ src/components/PlatformScreen/MiniLeaderboard.js
@@ -69,41 +69,26 @@ export default function MiniLeaderboard({ width, platformName, leaderboardEntrie
 
   return (
     <Box sx={{ width: width }}>
-      <Box sx={{paddingLeft: "5%"}}>
+      <Box>
         <Typography sx={{fontSize:"20px", fontWeight: "bold"}}>
           Leaderboards
         </Typography>
       </Box>
-      <Box sx={{paddingLeft: "5%"}}>
-        <Button onClick={()=>history.push(`/leaderboard/${platformName}`)} sx={{fontSize:"15px", color: "blue"}}>
+      <Box>
+        <Button onClick={()=>history.push(`/leaderboard/${platformName}`)} sx={{fontSize:"15px", color: "blue", paddingLeft: "0px"}}>
             View Full Leaderboard &gt;&gt;
         </Button>
       </Box>
-      <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-        <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
-          <Tab label="Top" sx={{fontSize:"12px", minWidth: `${20/2}px`}} {...a11yProps(0)} />
-          <Tab label="Rising" sx={{fontSize:"12px", minWidth: `${20/2}px`}} {...a11yProps(1)} />
-          <Tab label="Past Week" sx={{fontSize:"12px"}} {...a11yProps(2)} />
-          <Tab label="Past Month" sx={{fontSize:"12px"}} {...a11yProps(3)} />
-        </Tabs>
-      </Box>
-      <TabPanel value={value} index={0}>
-        <Grid container sx={{width}} spacing={2}>
-          {leaderboardEntries?leaderboardEntries.map((leaderBoardEntry,i)=>{if (i < 10) {return <MiniLeaderboardRow key={i} avatar={leaderBoardEntry.avatar} score={leaderBoardEntry.secondaryScore} username={leaderBoardEntry.username} position={i}/>}}):<Grid item xs={12}>Nobody has played any quizzes yet!</Grid>}
+      <Typography>
+      <Grid container sx={{width}} spacing={2}>
+        <Grid item container xs={12} spacing={0}>
+          <Grid item xs={1}>#</Grid>
+          <Grid item xs={7}>User</Grid>
+          <Grid item xs={4}>Score</Grid>
         </Grid>
-      </TabPanel>
-      <TabPanel value={value} index={1}>
-        Nobody has played any quizzes yet!
-      </TabPanel>
-      <TabPanel value={value} index={2}>
-        Nobody has played any quizzes yet!
-      </TabPanel>
-      <TabPanel value={value} index={3}>
-        Nobody has played any quizzes yet!
-      </TabPanel>
-      <TabPanel value={value} index={4}>
-        Nobody has played any quizzes yet!
-      </TabPanel>
+        {leaderboardEntries?leaderboardEntries.map((leaderBoardEntry,i)=>{if (i < 10) {return <MiniLeaderboardRow key={i} avatar={leaderBoardEntry.avatar} score={leaderBoardEntry.secondaryScore} username={leaderBoardEntry.username} position={i}/>}}):<Grid item xs={12}>Nobody has played any quizzes yet!</Grid>}
+      </Grid>
+      </Typography>
     </Box>
   );
 }
