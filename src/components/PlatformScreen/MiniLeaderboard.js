3958a3e2c7fffbe3dde2d0c6a09d4b320a0c7e15
John Park <loopuntiltrue@gmail.com>
2021-12-10 19:14:32 -0500

    Slight UI tweaks for MiniLeaderboard

--- src/components/PlatformScreen/MiniLeaderboard.js
+++ src/components/PlatformScreen/MiniLeaderboard.js
@@ -81,10 +81,10 @@ export default function MiniLeaderboard({ width, platformName, leaderboardEntrie
       </Box>
       <Typography>
       <Grid container sx={{width}} spacing={2}>
-        <Grid item container xs={12} spacing={0}>
+        <Grid item container xs={12} spacing={0} sx={{borderBottom: "1px solid lightgray"}}>
           <Grid item xs={1}>#</Grid>
           <Grid item xs={7}>User</Grid>
-          <Grid item xs={4}>Score</Grid>
+          <Grid item xs={2} sx={{position:"relative", left:"10px"}}>Score</Grid>
         </Grid>
         {leaderboardEntries?leaderboardEntries.map((leaderBoardEntry,i)=>{if (i < 10) {return <MiniLeaderboardRow key={i} avatar={leaderBoardEntry.avatar} score={leaderBoardEntry.secondaryScore} username={leaderBoardEntry.username} position={i}/>}}):<Grid item xs={12}>Nobody has played any quizzes yet!</Grid>}
       </Grid>
