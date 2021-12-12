0bb63a4f03877a2d06f4869a4b0e20c6b3615f61
Tyler Htet Naing Phyo <42888726+TylerHNP@users.noreply.github.com>
2021-12-11 19:50:14 -0500

    fix for platform screen

--- src/components/ProfileScreen/Friends.js
+++ src/components/ProfileScreen/Friends.js
@@ -7,16 +7,15 @@ import { useState } from "react";
 import { globalState } from "../../state/UserState";
 import { GET_USER_FRIENDS_INFO } from '../../controllers/graphql/user-queries';
 import { useQuery } from "@apollo/client";
-export default function Friends({ userId }) {
+export default function Friends({ userId, isOwn }) {
     const theme = useTheme();
-    const [friendRequests, setFriendRequests] = useState([]);
-    const [friends, setFriends] = useState([]);
+    let friends = [];
+    let friendRequests = []
 
     const { data, loading } = useQuery(GET_USER_FRIENDS_INFO, { variables: { userId: userId } })
 
 
     if (loading) {
-
         return (
             <Grid container justifyContent='center' alignItems='center' sx={{ height: '40vh', width: '100%' }}>
                 <CircularProgress variant='indeterminate' color='primary' />
@@ -27,6 +26,8 @@ export default function Friends({ userId }) {
 
     if (data) {
         console.log(data);
+        friends = data.getUserFriendsInfo.friendsInfo;
+        friendRequests = data.getUserFriendsInfo.friendRequestsInfo;
     }
 
 
@@ -37,39 +38,41 @@ export default function Friends({ userId }) {
                     '& svg': { fill: theme.palette.primary.main }
                 }
             }}>
-            <Paper
-                elevation={0}
-                component="form"
-                sx={{
-                    px: 2,
-                    display: 'flex',
-                    alignItems: 'center',
-                    width: '100%',
-                    height: '36px',
-                    maxWidth: '500px',
-                    background: theme.palette.grey[200],
-
-                }}
-            >
-                <InputBase
+            {isOwn ? <>
+                <Paper
+                    elevation={0}
+                    component="form"
                     sx={{
-                        ml: 1, flex: 1, fontSize: 14, fontWeight: 500,
+                        px: 2,
+                        display: 'flex',
+                        alignItems: 'center',
+                        width: '100%',
+                        height: '36px',
+                        maxWidth: '500px',
+                        background: theme.palette.grey[200],
+
                     }}
-                    placeholder="Enter username"
-                    inputProps={{ 'aria-label': 'search Yway' }}
-                />
-                <IconButton type="submit" sx={{ p: '3px' }} aria-label="search">
-                    <SearchRounded sx={{ fill: theme.palette.grey['500'] }} />
-                </IconButton>
-            </Paper>
+                >
+                    <InputBase
+                        sx={{
+                            ml: 1, flex: 1, fontSize: 14, fontWeight: 500,
+                        }}
+                        placeholder="Enter username"
+                        inputProps={{ 'aria-label': 'search Yway' }}
+                    />
+                    <IconButton type="submit" sx={{ p: '3px' }} aria-label="search">
+                        <SearchRounded sx={{ fill: theme.palette.grey['500'] }} />
+                    </IconButton>
+                </Paper>
+                <CommonTitle title='FRIEND REQUESTS' />
+                <Grid container justifyContent='flex-start' mb={1}>
+                    {friendRequests.length === 0 ?
+                        <Typography>No friend requests</Typography> : friendRequests.map((data) => <NotificationCard
+                            key={data._id}{...data} />)}
 
-            <CommonTitle title='FRIEND REQUESTS' />
-            <Grid container justifyContent='flex-start' mb={1}>
-                {friendRequests.length === 0 ?
-                    <Typography>No friend requests</Typography> : friendRequests.map((data) => <NotificationCard
-                        key={data._id}{...data} />)}
-
-            </Grid>
+                </Grid>
+            </> : null
+            }
             <CommonTitle title='FRIENDS' />
             <Grid container justifyContent='flex-start' mb={1}>
                 {friends.length === 0 ? <Typography>No friends</Typography> : friends.map((data) => <FriendCard
