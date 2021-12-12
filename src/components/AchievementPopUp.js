d394d914784ba9894c59385ca15835d3f0768a94
Tyler Htet Naing Phyo <42888726+TylerHNP@users.noreply.github.com>
2021-12-12 15:58:46 -0500

    route achievement pop up check out to user's achievements

--- src/components/AchievementPopUp.js
+++ src/components/AchievementPopUp.js
@@ -1,6 +1,11 @@
 import { Dialog, Avatar, Stack, Typography, Button } from '@mui/material';
+import { useHistory } from 'react-router';
+import { globalState } from '../state/UserState';
+import { useReactiveVar } from '@apollo/client';
 
 export default function AchievementPopUp({ icon, name, description, open, handleClose, checkItOut }) {
+    const history = useHistory();
+    const user = useReactiveVar(globalState);
     return (
         <Dialog open={open} onClose={handleClose}
             aria-labelledby="quiz-details-dialog" sx={{ backgroundColor: 'transparent' }} >
@@ -10,8 +15,9 @@ export default function AchievementPopUp({ icon, name, description, open, handle
                 <Avatar src={icon} sx={{ height: 100, width: 100, mt: 8, mb: 2 }} />
                 <Typography sx={{ fontSize: 20, fontWeight: 700, color: 'primary.main' }}> {name}</Typography>
                 <Typography textAlign='center' sx={{ fontSize: 14, fontWeight: 500, color: 'grey.500', mt: 1 }}> {description}   </Typography>
-                <Button variant='contained' sx={{ mt: 7 }}> CHECK IT OUT</Button>
+                <Button variant='contained' sx={{ mt: 7 }}
+                    onClick={() => { history.push(`/user/${user._id}/achievements`) }}
+                > CHECK IT OUT</Button>
             </Stack>
-
         </Dialog >);
 }
