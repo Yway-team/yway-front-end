a7304c1847d7457d32f41bedf2c862a8021dd0b6
Tyler Htet Naing Phyo <42888726+TylerHNP@users.noreply.github.com>
2021-12-13 05:07:26 -0500

    handle remove friend errror of not closing the pop up

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -245,7 +245,10 @@ export default function ProfileScreen() {
                 title='REMOVE FRIEND'
                 content={`Are you sure you want to remove this friend? `}
                 yesText='REMOVE FRIEND'
-                yesCallback={() => { handleRemoveFriend(); }}
+                yesCallback={() => {
+                    setRemoveFriendOpen(false);
+                    handleRemoveFriend();
+                }}
                 noText='CANCEL'
                 noCallback={() => { setRemoveFriendOpen(false); }}
             />
