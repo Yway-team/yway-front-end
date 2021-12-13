0b955e07323493cbf8992db336ddd96d50eab134
Fletcher Ramee <fletcher.ramee@stonybrook.edu>
2021-12-13 06:56:57 -0500

    More grammatical adjustments

--- src/components/AchievementPopUp.js
+++ src/components/AchievementPopUp.js
@@ -10,8 +10,8 @@ export default function AchievementPopUp({ icon, name, description, open, handle
         <Dialog open={open} onClose={handleClose}
             aria-labelledby="quiz-details-dialog" sx={{ backgroundColor: 'transparent' }} >
             <Stack sx={{ width: 350, height: 400, p: 4, px: 6 }} alignItems='center'>
-                <Typography sx={{ fontSize: 20, fontWeight: 700, color: 'primary.black' }}> Congraulations!  </Typography>
-                <Typography sx={{ fontSize: 14, fontWeight: 500, color: 'grey.500' }}> You just won a new achievement.   </Typography>
+                <Typography sx={{ fontSize: 20, fontWeight: 700, color: 'primary.black' }}> Congratulations!  </Typography>
+                <Typography sx={{ fontSize: 14, fontWeight: 500, color: 'grey.500' }}> You've earned a new achievement.   </Typography>
                 <Avatar src={icon} sx={{ height: 100, width: 100, mt: 8, mb: 2 }} />
                 <Typography sx={{ fontSize: 20, fontWeight: 700, color: 'primary.main' }}> {name}</Typography>
                 <Typography textAlign='center' sx={{ fontSize: 14, fontWeight: 500, color: 'grey.500', mt: 1 }}> {description}   </Typography>
